21b33e74ff43c2acdbf257d35af7825282d50613
Xopher <xopher@vi.co>
2026-04-14 21:30:24 -0700

    chore: sync plugins

--- xophz-compass-magic-wand.php
+++ xophz-compass-magic-wand.php
@@ -19,7 +19,7 @@
  * Description:       Make SEO effortlessly enchanting by visually managing your meta data, sitemaps, and crawls.
  * Version:           26.4.14.1311
  * Author:            Hall of the Gods, Inc.
- * Author URI:        http://www.midnightnerd.com/
+ * Author URI:        http://www.hallofthegods.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       xophz-compass-magic-wand
